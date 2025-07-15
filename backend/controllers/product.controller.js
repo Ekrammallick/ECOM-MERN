a23a26bef98099684d983179acbb76d3123e6997
Ekram Mallick <ekrammallick18@gmail.com>
2025-07-15 22:06:10 +0530

    Featured Product Conttoler Added

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -8,3 +8,20 @@ export const getAllProducts = async (req,res)=>{
         res.status(500).json({message:"server error",error:error.message})
     }
 }
+
+export const getFeaturedProducts = async (req,res)=>{
+    try {
+        let featuredProducts = await redis.get("featured_products");
+         if(featuredProducts){
+            return res.json(JSON.parse(featuredProducts));
+         }
+         featuredProducts = await Product.find({isFeature:true}).lean();
+        if(!featuredProducts){
+            return res.status(404).json({message:"no featured products found"});
+        }
+        await redis.set("feature_products",JSON.stringify(featuredProducts));
+    } catch (error) {
+        console.log("Error in getFeatureProducts controller",error.message);
+        res.status(500).json({message:"server error",error:error.message})
+    }
+}
