d7a6fad51420d562da87ee29982c623d16816c73
Ekram Mallick <ekrammallick18@gmail.com>
2025-07-18 21:50:12 +0530

    18 July : payment Routes and Controllers added

--- backend/routes/payment.route.js
+++ backend/routes/payment.route.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
+import { protectRoute } from "../middlewares/auth.middleware";
 
+import { checkoutSuccess, createCheckoutSession } from "../controllers/payment.controller";
 const router = Router();
 
-router.post("/create-checkout-session",)
-
+router.post("/create-checkout-session", protectRoute,createCheckoutSession);
+router.post("/checkout-success", protectRoute,checkoutSuccess);
 
 export default router;
-
-
---
+++ backend/utils/stripe.utils.js
@@ -0,0 +1,18 @@
+
+
+export async function createStripeCoupon(discountPercentage) {
+  const coupon = await stripe.coupons.create({
+    percent_off: discountPercentage,
+    duration: "once",
+  });
+  return coupon.id;
+}
+ export async function createNewCoupon(userId){
+    const newCoupon = new Coupon({
+        code:"GIFT" + Math.random().toString(36).substring(2,8).toUpperCase(),
+        discountPercentage:10,
+        expirationDate:new Date (Date.now() + 30*24*60*60*1000), //30 days from now
+        userId:userId
+    })
+    await newCoupon.save();
+}
