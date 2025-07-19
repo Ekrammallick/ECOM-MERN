20b562da9949ce92c6c824693c57b27c1dc6cf58
Ekram Mallick <ekrammallick18@gmail.com>
2025-07-19 20:31:20 +0530

    july 19 : frontend setup complete

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+
+function app(){
+// const [count,setCount] = useState(0);
+return(
+  <>
+  <h1 className="text-red-500">Hello world</h1>
+  </>
+)
+}
+
+export default app
