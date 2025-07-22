fedea3f842c022eb3c6483b895c0b010edf163b6
Ekram Mallick <ekrammallick18@gmail.com>
2025-07-22 18:56:17 +0530

    July 22 : Added the Navbar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
-import { useState } from "react"
+import {Routes,Route} from "react-router-dom";
+import { HomePage } from "./pages/HomePage"
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
 
 function app(){
 // const [count,setCount] = useState(0);
 return(
-  <>
-  <h1 className="text-red-500">Hello world</h1>
-  </>
+  <div>
+    
+    <Routes>
+      <Route path="/" element={<HomePage/>} />
+      <Route path="/signup" element={<SignUpPage/>} />
+      <Route path="/login" element={<LoginPage/>} />
+    </Routes>
+  </div>
 )
 }
 
